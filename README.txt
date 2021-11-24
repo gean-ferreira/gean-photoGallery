3f0974324cf30e61bc477d8c719b8b3af4fb04dc
geanroberto <gean_roberto@hotmail.com>
2021-11-24 18:56:23 -0300

    changed and installed dependencies

---
+++ README.txt
@@ -0,0 +1,7 @@
+
+Deployed: ok;
+Date: 11/24/2021;
+
+For install dependencies:
+npm install
+
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const modoDev = process.env.NODE_ENV !== 'production'
+const webpack = require('./node_modules/webpack')
+const MiniCssExtractPlugin = require('./node_modules/mini-css-extract-plugin')
+const UglifyJsPlugin = require('./node_modules/uglifyjs-webpack-plugin')
+const OptimizeCSSAssetsPlugin = require('./node_modules/optimize-css-assets-webpack-plugin')
+const CopyWebpackPlugin = require('./node_modules/copy-webpack-plugin')
+
+module.exports = {
+    mode: modoDev ? 'development' : 'production',
+    entry: './src/index.js',
+    devServer: {
+        contentBase: './build',
+        port: 8081,
+    },
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true,
+                sourceMap: true
+            }),
+            new OptimizeCSSAssetsPlugin({})
+        ]
+    },
+    output: {
+        filename: 'app.js',
+        path: __dirname + '/build'
+    },
+    plugins: [
+        new MiniCssExtractPlugin({ filename: 'style.css' }),
+        new CopyWebpackPlugin([
+            { context: 'src/', from: '**/*.html' },
+            { context: 'src/', from: 'imgs/**/*' }
+        ])
+    ],
+    module: {
+        rules: [{
+            test: /\.s?[ac]ss$/,
+            use: [
+                MiniCssExtractPlugin.loader,
+                // 'style-loader', // Adiciona CSS a DOM injetando a tag <style>
+                'css-loader', // interpreta @import, url()...
+                'sass-loader',
+            ]
+        }, {
+            test: /\.(png|svg|jpg|gif)$/,
+            use: ['file-loader']
+        }, {
+            test: /.(ttf|otf|eot|svg|woff(2)?)$/,
+            use: ['file-loader']
+        }]
+    }
+}

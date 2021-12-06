b9e76a7cd2be414c6a2d32a91263e72b83b9455b
Gean Ferreira <gean_roberto@hotmail.com>
2021-12-06 12:12:08 -0300

    photo request now with jquery. Same functionality and less code

--- src/js/puglins/modal.js
+++ src/js/puglins/modal.js
@@ -1,22 +1,11 @@
-document.body.onmousedown = function(){
-  var idImg = event.target.attributes[0].value
-  const img = document.getElementById(idImg)
+import $ from "jquery";
 
-  var modal = document.getElementById('myModal');
-  var modalImg = document.getElementById("img01");
-  var captionText = document.getElementById("caption");
-
-  img.onmouseup = function(){
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
-  }
-
-  // Get the <span> element that closes the modal
-  var span = document.getElementsByClassName("close")[0];
-
-  // When the user clicks on <span> (x), close the modal
-  span.onclick = function() {
-    modal.style.display = "none";
-  }
-}
+$("body").on("click", "img", function(event) {
+  $('#myModal').css("display", "block")
+  $("#img01").attr("src", event.target.src)
+  $("#caption").append(event.target.alt)
+  $(".close").on("click", function(){
+    $("#caption").empty()
+    $('#myModal').css("display", "none")
+  })
+});
